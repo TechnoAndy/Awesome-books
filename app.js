d8672b9f724dc79edc0d771454a88437d1168d27
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-09-19 22:54:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,39 @@ class Book {
     this.author = author;
   }
 }
-
+// Store Class: Handles Storage
+class Store {
+    static getBooks() {
+      let books;
+      if (localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+  
+      books.push(book);
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(title) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if (book.title === title) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
 // UI Class: Handle UI Tasks
 
 class UI {
@@ -42,39 +74,7 @@ class UI {
   }
 }
 
-// Store Class: Handles Storage
-class Store {
-  static getBooks() {
-    let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
 
-    return books;
-  }
-
-  static addBook(book) {
-    const books = Store.getBooks();
-
-    books.push(book);
-
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static removeBook(title) {
-    const books = Store.getBooks();
-
-    books.forEach((book, index) => {
-      if (book.title === title) {
-        books.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
