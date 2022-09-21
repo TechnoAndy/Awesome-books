626d26c868e363e73dffceb54cf2b1ae11edbf2d
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-09-21 14:57:21 +0200

    Add complete website with navigation
    Add navigation, contact and with functionality

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-classes-per-file */
 
+const listBtn = document.querySelector('#listBtn');
+const addNewBtn = document.querySelector('#addNewBtn');
+const contactBtn = document.querySelector('#contactBtn');
+
+const listSec = document.querySelector('.list');
+const addNewSec = document.querySelector('.addNew');
+const contactSec = document.querySelector('.contact');
+
+const addBtn = document.querySelector('#addBtn');
+
 // Book Class: Represents a book
 class Book {
   constructor(title, author) {
@@ -123,4 +133,38 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
     e.target.parentElement.previousElementSibling.previousElementSibling
       .textContent,
   );
+});
+
+listBtn.addEventListener('click', () => {
+  listBtn.classList.add('active');
+  addNewBtn.classList.remove('active');
+  contactBtn.classList.remove('active');
+  listSec.style.display = 'block';
+  addNewSec.style.display = 'none';
+  contactSec.style.display = 'none';
+});
+
+addNewBtn.addEventListener('click', () => {
+  listBtn.classList.remove('active');
+  addNewBtn.classList.add('active');
+  contactBtn.classList.remove('active');
+  listSec.style.display = 'none';
+  addNewSec.style.display = 'block';
+  contactSec.style.display = 'none';
+});
+
+contactBtn.addEventListener('click', () => {
+  listBtn.classList.remove('active');
+  addNewBtn.classList.remove('active');
+  contactBtn.classList.add('active');
+  listSec.style.display = 'none';
+  addNewSec.style.display = 'none';
+  contactSec.style.display = 'flex';
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    addBtn.click();
+  }
 });
